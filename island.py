4f00066a29988f5c1148d25118eef37f30df2a16
Tayu0404 <tkt.yusei0404@gmail.com>
2019-06-12 09:38:17 +0900

    first commit

---
+++ island.py
@@ -0,0 +1,23 @@
+import random
+import sys
+islands = {
+        "軍艦島" : 6,
+        "屋久島" : 6,
+        "小豆島" : 6,
+        "種子島" : 6,
+        "佐渡島" : 6
+        }
+
+def selectIsland () :
+    selectIsland = random.choice(list(islands.keys()))
+    islands[selectIsland] -= 1
+    print(selectIsland) #select island
+    if islands[selectIsland] == 0:
+        del islands[selectIsland]
+
+for i in range(1, 255):
+    try:
+        selectIsland()
+    except Exception:
+        print('Done')
+        sys.exit()
